30dd79705e99bfe37c387a9869ca7e1aae6163b2
tallybee <talbondareva@gmail.com>
2019-07-27 15:27:29 +0200

    added cors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const { send } = require("micro")
 const query = require("micro-query")
+const cors = require('micro-cors')()
 
-module.exports = (req, res) => {
+module.exports = cors((req, res) => {
 	const queryParams = query(req)
 	const result = Array(parseInt(queryParams.numberRequired, 10))
 		.fill(0)
 		.map(() => Math.ceil(Math.random() * queryParams.max))
 	send(res, 200, result)
-}
+})
