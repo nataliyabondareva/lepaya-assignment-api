f576b3de3c54f07fe017dd0bd12fe66fbc33cc9e
tallybee <talbondareva@gmail.com>
2019-07-29 22:53:30 +0200

    actual unique

--- api/index.js
+++ api/index.js
@@ -4,9 +4,8 @@ const cors = require('micro-cors')()
 
 module.exports = cors((req, res) => {
 	const queryParams = query(req)
-	const result = new Set;
-	const result = Array(parseInt(queryParams.numberRequired, 10))
-		.fill(0)
-		.map(() => Math.ceil(Math.random() * queryParams.max))
-	send(res, 200, result)
-})
+ const result =Array(parseInt(queryParams.numberRequired, 10))
+	 .fill(0)
+	 .map(() => Math.ceil(Math.random() * queryParams.max))
+	send(res, 200, [...new Set(result)]);
+ })
