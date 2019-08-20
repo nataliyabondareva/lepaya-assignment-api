d537fea3ab5adfd29e885174351f706812af412b
tallybee <talbondareva@gmail.com>
2019-08-20 22:28:47 +0200

    now always sends back requested number

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
-const { send } = require("micro")
-const query = require("micro-query")
+const { send } = require('micro')
+const query = require('micro-query')
 const cors = require('micro-cors')()
 
 module.exports = cors((req, res) => {
 	const queryParams = query(req)
- const result =Array(parseInt(queryParams.numberRequired, 10))
-	 .fill(0)
-	 .map(() => Math.ceil(Math.random() * queryParams.max))
-	send(res, 200, [...new Set(result)]);
- })
+	const result = new Set()
+  while (result.size < queryParams.numberRequired) {
+    result.add(Math.ceil(Math.random() * queryParams.max));
+	}
+	send(res, 200, Array.from(result))
+});
